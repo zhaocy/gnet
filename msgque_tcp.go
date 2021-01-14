4dcfcf250eae3431c6a3718975dc381289ed8922
zhaocy <sh_zcy@qq.com>
2021-01-14 18:21:18 +0800

    update

--- msgque_tcp.go
+++ msgque_tcp.go
@@ -364,7 +364,6 @@ func (r *tcpMsgQue) listen() {
 						msgque.write()
 						LogInfo("process write end for msgque:%d", msgque.id)
 					})
-					msgque.Send(NewDataMsg([]byte("I get you !!!")))
 				} else {
 					msgque.Stop()
 				}
