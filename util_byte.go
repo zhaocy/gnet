7fe33464caee3b9060021f708a46567a6d8ba07c
zhaocy <sh_zcy@qq.com>
2021-01-14 17:03:32 +0800

    update  util_byte

--- util_byte.go
+++ util_byte.go
@@ -1,6 +1,9 @@
 package gnet
 
-import "encoding/binary"
+import (
+	"bytes"
+	"encoding/binary"
+)
 
 func Uint16ToBytes(n uint16) []byte {
 	return []byte{
@@ -11,4 +14,12 @@ func Uint16ToBytes(n uint16) []byte {
 
 func BytesToUint16(n []byte) uint16 {
 	return binary.BigEndian.Uint16(n)
+}
+
+
+func BytesConnect(b1,b2 []byte)[]byte{
+	var buffer bytes.Buffer //Buffer是一个实现了读写方法的可变大小的字节缓冲
+	buffer.Write(b1)
+	buffer.Write(b2)
+	return buffer.Bytes()  //得到了b1+b2的结果
 }
