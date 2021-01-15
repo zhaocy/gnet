adb147fa2a55cae7a5980a36d495a8255ee6bbdb
zhaocy <sh_zcy@qq.com>
2021-01-15 15:52:23 +0800

    update

--- parser_custom.go
+++ parser_custom.go
@@ -14,23 +14,40 @@ func (r *CustomParser) ParseC2S(msg *Message) (IMsgParser, error) {
 		return nil, ErrCustomUnPack
 	}
 
-	if msg.Head == nil {
-		if len(msg.Data) == 0 {
-			return nil, ErrCustomUnPack
-		}
-		for _, p := range r.typeMap {
-			if p.C2S() != nil {
-				fmt.Println("ParseC2S--------------")
-				err := CustomUnPack(msg.Data, p.C2S())
-				if err != nil {
-					continue
-				}
-				p.parser = r
-				return &p, nil
-			}
-		}
-
+	if len(msg.Data) == 0 {
+		return nil, ErrCustomUnPack
 	}
+
+	headData := make([]byte, MsgShortHeadSize)
+	var data []byte
+	var head *MessageShortHead
+
+	if head = NewMessageShortHead(headData); head == nil {
+		LogError("short read msg head failed")
+		return nil, ErrCustomUnPack
+	}
+	msg.ShortHead = head
+	if head.Len > 0 {
+		data = make([]byte, head.Len)
+		data = msg.Data[MsgShortHeadSize:]
+		msg.Data = data
+	}else{
+		return nil, ErrCustomUnPack
+	}
+
+	for _, p := range r.typeMap {
+		if p.C2S() != nil {
+			fmt.Println("ParseC2S--------------")
+			err := CustomUnPack(msg.Data, p.C2S())
+			if err != nil {
+				continue
+			}
+			p.parser = r
+			return &p, nil
+		}
+	}
+
+
 
 	return nil, ErrCustomUnPack
 }
