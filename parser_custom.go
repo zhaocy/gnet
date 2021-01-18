9b5dedd395896c9287963bfa213c1a60db900aab
zhaocy <sh_zcy@qq.com>
2021-01-18 13:50:35 +0800

    update

--- parser_custom.go
+++ parser_custom.go
@@ -29,14 +29,12 @@ func (r *CustomParser) ParseC2S(msg *Message) (IMsgParser, error) {
 
 	LogDebug("head %v", headData)
 	msg.ShortHead = head
-	if head.Len > 0 {
-		data = make([]byte, head.Len)
-		data = msg.Data[MsgShortHeadSize:]
-		msg.Data = data
-		LogDebug("data len: %v %v",head.Len, msg.Data)
-	}else{
-		return nil, ErrCustomUnPack
-	}
+
+	data = make([]byte, head.Len)
+	data = msg.Data[MsgShortHeadSize:]
+	msg.Data = data
+	LogDebug("data len: %v %v",head.Len, msg.Data)
+
 
 	for _, p := range r.typeMap {
 		if p.C2S() != nil {
