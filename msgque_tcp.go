44750512afa4fc7749afa429a570352693be3b14
zhaocy <sh_zcy@qq.com>
2021-02-05 10:11:07 +0800

    update

--- msgque_tcp.go
+++ msgque_tcp.go
@@ -78,7 +78,6 @@ func (r *tcpMsgQue) readMsgCustom() {
 	var err error
 	var len int
 	var scanner *bufio.Scanner
-	//var data []byte
 	for !r.IsStop() {
 		if r.rawBuffer != nil {
 			result := bytes.NewBuffer(nil)
@@ -88,26 +87,17 @@ func (r *tcpMsgQue) readMsgCustom() {
 				scanner = bufio.NewScanner(result)
 				scanner.Split(r.packetSlitFunc)
 			}
-		} /*else {
-			data, err = reader.ReadBytes('\n')
-		}*/
+		}
 		if err != nil {
 			break
 		}
 
-		//if scanner.Scan(){
-			for scanner.Scan(){
-				if !r.processMsg(r, &Message{Data: scanner.Bytes()}) {
-					break
-				}
+		for scanner.Scan(){
+			if !r.processMsg(r, &Message{Data: scanner.Bytes()}) {
+				break
 			}
-		//}else{
-		//	data = make([]byte, len)
-		//	copy(data, r.rawBuffer)
-		//	if !r.processMsg(r, &Message{Data: data}) {
-		//		break
-		//	}
-		//}
+		}
+
 
 
 		r.lastTick = Timestamp
